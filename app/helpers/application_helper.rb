a39df1388f837db616b0a4e3abdd3b2981710f8e
Edvardas <lt93970@gmail.com>
2022-11-23 23:07:50 +0200

    rubocop style fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,6 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def full_title(page_title = '')
-    base_title = 'Plank'
-    if page_title.present?
-      "#{page_title} | #{base_title} "
-    else
-      base_title
-    end
-  end
-
-  def currently_at(current_page = '')
-    render partial: 'shared/menu', locals: { current_page: current_page }
-  end
-
   def nav_tab(title, url, options = {})
     current_page = options.delete :current_page
     css_class = current_page == title ? 'active' : ''
@@ -27,5 +14,18 @@ module ApplicationHelper
                       end
 
     link_to title, url, options
+  end
+
+  def currently_at(current_page = '')
+    render partial: 'shared/menu', locals: { current_page: current_page }
+  end
+
+  def full_title(page_title = '')
+    base_title = 'Plank'
+    if page_title.present?
+      "#{page_title} | #{base_title} "
+    else
+      base_title
+    end
   end
 end
